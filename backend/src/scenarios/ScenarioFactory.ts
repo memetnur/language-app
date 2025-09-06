6e6b1af60805a93f80dd3d700eee2603f8caa9a4
Nur memeti <57399386+memetnur@users.noreply.github.com>
2025-09-06 14:17:28 +0200

    Update ScenarioFactory.ts

--- backend/src/scenarios/ScenarioFactory.ts
+++ backend/src/scenarios/ScenarioFactory.ts
@@ -1,1 +1,20 @@
-.
+import { TouristInfoScenario } from './tourist'
+import { HotelReceptionScenario } from './hotel'
+import { TrainStationScenario } from './train'
+import { DentistScenario} from './Dentist'
+import { Scenario } from './Scenario'
+
+export class ScenarioFactory {
+  static createScenario(scenario: string): Scenario {
+    switch (scenario) {
+      case 'supermarket':
+        return new SupermarketScenario()
+      case 'restaurant':
+        return new RestaurantScenario()
+      case 'train station':
+        return new TrainStationScenario()
+      default:
+        throw new Error(`Scenario "${scenario}" not found.`)
+    }
+  }
+}
